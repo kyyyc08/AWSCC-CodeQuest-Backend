eb34555504be27d2731a1b7a09089ad0d30c2142
kyyyc08 <116141726+kyyyc08@users.noreply.github.com>
2023-12-29 15:53:22 +0800

    Add files via upload

---
+++ backend/day-01/day1Challenge_Caras.py
@@ -0,0 +1,6 @@
+your_name = "Kyrelle cute"
+name_of_department = "DSWD"
+about_you = "I love SEVENTEEN especially DK so much and I need Yunjin from Le Sserafim to marry me right now < 3"
+
+print(f"""Hello, I am {your_name}. I love {name_of_department} and I love being in the Backend even more!
+One thing about me is that {about_you}.""")
