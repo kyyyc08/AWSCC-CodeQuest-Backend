691fe796eea871118def55a7c288d344e3ba71f4
kyyyc08 <116141726+kyyyc08@users.noreply.github.com>
2023-12-29 16:15:47 +0800

    day2Input_Caras.py

---
+++ backend/day-02/day-2.py
@@ -0,0 +1,7 @@
+print("\nEnter three non-negative integers to find their sum.")
+firstNumber = int(input("Enter the first number: "))
+secondNumber = int(input("Enter the second number: "))
+thirdNumber = int(input("Enter the third number: "))
+sum = firstNumber + secondNumber + thirdNumber
+
+print(f"\nSum: {sum}\n")
